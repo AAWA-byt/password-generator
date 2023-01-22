0f26ad89a015882a33b7f8082eb5ba6dc40e14f7
AAWA-byt <mail@aaronwagner.earth>
2023-01-22 22:30:02 +0100

    :sparkles: add code for generating password

---
+++ script.js
@@ -0,0 +1,28 @@
+// get interact
+var passwd_field = document.getElementById("passwd");
+
+
+// functions for generatePasswd()
+const generateSecurePassword = () => {
+    // Set password length and characters to use
+    const length = 16;
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:\'",.<>?/\\';
+
+    // Use crypto.getRandomValues() to generate a cryptographically secure random password
+    const array = new Uint32Array(length);
+    window.crypto.getRandomValues(array);
+    let password = '';
+    for (let i = 0; i < array.length; i++) {
+        password += characters.charAt(array[i] % characters.length);
+    }
+
+    // Insert the password into the text field
+    document.getElementById('passwd').value = password;
+}
+
+
+// functions for copyPasswd() 
+function copyPasswd() {
+    navigator.clipboard.writeText(passwd_field.value);
+}
+
