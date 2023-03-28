0d0a3e89c0153dd536f87e20fa3a8abbfa95d3e9
Aaron Wagner <76434239+AAWA-byt@users.noreply.github.com>
2023-03-28 10:44:58 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// get interact
+// This code gets a reference to the password field
 var passwd_field = document.getElementById("passwd");
 
-
-// functions for generatePasswd()
+// This function generates a secure random password
 const generateSecurePassword = () => {
-    // Set password length and characters to use
-    const length = 16;
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:\'",.<>?/\\';
+// Defines the length of the password
+const length = 16;
+// Defines the characters that can be used in the password
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:'",.<>?/\';
 
-    // Use crypto.getRandomValues() to generate a cryptographically secure random password
-    const array = new Uint32Array(length);
-    window.crypto.getRandomValues(array);
-    let password = '';
-    for (let i = 0; i < array.length; i++) {
-        password += characters.charAt(array[i] % characters.length);
-    }
+// Generates an array of random numbers using the Web Crypto API
+const array = new Uint32Array(length);
+window.crypto.getRandomValues(array);
 
-    // Insert the password into the text field
-    document.getElementById('passwd').value = password;
+// Builds the password string using the random numbers and the defined characters
+let password = '';
+for (let i = 0; i < array.length; i++) {
+    password += characters.charAt(array[i] % characters.length);
 }
 
+// Sets the generated password as the value of the password field
+document.getElementById('passwd').value = password;
 
-// functions for copyPasswd() 
+}
+
+// This function copies the generated password to the clipboard
 function copyPasswd() {
-    navigator.clipboard.writeText(passwd_field.value);
-    alert('Generated password was copied to clipboard')
+navigator.clipboard.writeText(passwd_field.value);
+alert('Generated password was copied to clipboard')
 }
-
